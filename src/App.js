fed30033c681b5699394ea987aa242fb325bc204
Andrey Krylov <a.krylov@8bitgroup.com>
2019-09-07 15:02:08 +0300

    добавил новые страницы, подключил библиотеку с роутингом

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import QuizList from './containers/QuizList/QuizList';
+import Auth from './containers/Auth/Auth';
+import QuizCreator from './containers/QuizCreator/Quiz-creator';
+
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Layout>
-        <Quiz/>
+        <Switch>
+          <Route path='/auth' component={Auth} />
+          <Route path='/quiz-creator' component={QuizCreator} />
+          <Route path='/quiz/:id' component={Quiz} />
+          <Route path='/' component={QuizList} />
+        </Switch>
       </Layout>
     </div>
   );
